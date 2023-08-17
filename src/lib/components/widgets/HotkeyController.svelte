0e041f029827fe19c3e9f10ffb8dd39a440bb47b
hylu-dev <roylu99@gmail.com>
2023-08-17 01:18:07 -0400

    Adjust gap

--- src/lib/components/widgets/HotkeyController.svelte
+++ src/lib/components/widgets/HotkeyController.svelte
@@ -57,6 +57,7 @@ function splitOffsetSubmit(e) {
         display: flex;
         justify-content: center;
         align-items: center;
+        gap: .2rem;
     }
 
     input[type=text], input[type=number] {
