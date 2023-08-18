65786202230b9472978e2dbecd7d7136d9f4de35
hylu-dev <roylu99@gmail.com>
2023-08-18 17:21:41 -0400

    Initial Commit

---
+++ src/lib/components/general/InputNumber.svelte
@@ -0,0 +1,48 @@
+<script>
+export let max = 1;
+export let min = 0;
+export let initial = 0;
+
+let dragDist = 1000;
+let value = initial;
+let startPos = 0;
+let isDragging = false;
+
+function trackValue(e) {
+    if (isDragging) {
+        let increment = ((e.clientY - startPos)/dragDist);
+        let delta = value - increment;
+        if (delta < min) {
+            //value = min;
+        } else if (delta > max) {
+            value = max;
+        } else {
+            value = delta;
+        }
+        
+    }
+}
+
+function handleMouseDown(e) {
+    isDragging = true;
+    startPos = e.clientY
+}
+
+function handleMouseUp(e) {
+    isDragging = false;
+}
+</script>
+
+<svelte:window on:mousemove={trackValue} on:mouseup={handleMouseUp}/>
+<input
+draggable="true"
+on:dragstart={handleMouseDown}
+bind:value={value}
+type="number">
+
+<style>
+    input[type='number'] {
+        cursor: n-resize;
+    }
+
+</style>
