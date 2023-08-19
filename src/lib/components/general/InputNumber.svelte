aa41562a22a69d11d4c9b0c29ca668b1ee15b7dd
hylu-dev <roylu99@gmail.com>
2023-08-18 22:00:41 -0400

    Add back input number spinner button

--- src/lib/components/general/InputNumber.svelte
+++ src/lib/components/general/InputNumber.svelte
@@ -45,4 +45,15 @@ type="number">
         cursor: n-resize;
     }
 
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+    -moz-appearance: textfield;
+}
 </style>
